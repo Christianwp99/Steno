706aa3b025c9a7a3d74b78133b8d04e7e3271cd1
FrederikLenz <lpfrede@gmail.com>
2025-05-15 11:52:37 +0200

    Afspil igen knap virker, mangler rigtig placering

--- Steno/js/crashtest.js
+++ Steno/js/crashtest.js
@@ -1,6 +1,18 @@
+let currentVideoSource = '';
+
 function playVideo(source) {
   const video = document.getElementById('videoPlayer');
-  video.src = 'video/' + source;
+  currentVideoSource = 'video/' + source; // Gem stien til aktuel video
+  video.src = currentVideoSource;
   video.load();
   video.play();
+}
+
+function replayVideo() {
+  const video = document.getElementById('videoPlayer');
+  if (currentVideoSource) {
+    video.src = currentVideoSource;
+    video.load();
+    video.play();
+  }
 }
